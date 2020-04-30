c3b494cf1a494c31e7a5920304f1641afc792ce9
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-04-30 19:16:59 +0300

    Fdroid audiorecorder

--- Recorder/Android.mk
+++ Recorder/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Recorder
-LOCAL_SRC_FILES := audioRecorder-unsigned.apk
+LOCAL_SRC_FILES := com.github.axet.audiorecorder_338.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
