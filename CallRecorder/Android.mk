00f354db08335ce05ced09b83c96831d2db93e2b
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-04-30 15:43:54 +0300

    2

--- CallRecorder/Android.mk
+++ CallRecorder/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := CallRecorder
-LOCAL_SRC_FILES := com.android.callrecorder_16.apk
+LOCAL_MODULE := callrecorder
+LOCAL_SRC_FILES := callrecorder.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
