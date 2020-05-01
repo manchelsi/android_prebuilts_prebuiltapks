e8644c778244ee60164a9c08d5bbfa4780859b8e
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-05-01 18:21:26 +0300

    Add com.github.axet.callrecorder

--- com.github.axet.callrecorder/Android.mk
+++ com.github.axet.callrecorder/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := CReco
+LOCAL_MODULE := com.github.axet.callrecorder
 LOCAL_SRC_FILES := com.github.axet.callrecorder_219.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
