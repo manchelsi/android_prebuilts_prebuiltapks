b6f03ea4140f54f082d4a6fe8a1c63c63deb1617
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-05-02 01:28:34 +0300

    Add packages

---
+++ it.niedermann.owncloud.notes/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := it.niedermann.owncloud.notes
+LOCAL_SRC_FILES := it.niedermann.owncloud.notes_2011012.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
