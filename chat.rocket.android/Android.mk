9f8a3f50ae7a4206c7235e3a4f7a4b0abb99cc80
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-05-02 10:45:24 +0300

    TST rocket version

--- chat.rocket.android/Android.mk
+++ chat.rocket.android/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := chat.rocket.android
-LOCAL_SRC_FILES := chat.rocket.android.2118.apk
+LOCAL_SRC_FILES := chat.rocket.android_2077.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
