0e31bb638fa4fb17d7e59b87db5af50d32bceb33
manchelsi <manchelsi@MacBook-Pro-manchelsi.local>
2020-05-05 10:47:41 +0300

    Add com.vrem.wifianalyzer_52.apk

---
+++ com.vrem.wifianalyzer/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := com.vrem.wifianalyzer
+LOCAL_SRC_FILES := com.vrem.wifianalyzer_52.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
